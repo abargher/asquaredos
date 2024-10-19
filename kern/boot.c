c1682515e43c9cf2e6b8cc62b5b27c4c12728ecb
gustrain <gus@waldspurger.com>
2024-10-19 18:30:57 -0500

    begin implementing process resource creation

--- kern/boot.c
+++ kern/boot.c
@@ -1,9 +1,14 @@
+#define KERNEL
+
 #include "boot.h"
 
 /* TODO use the real value from the SDK. */
 #define KB 1024
 #define SRAM_SIZE 256 * KB
 #define SRAM_START 0x20000000
+
+/* TODO find good value. */
+#define STACK_SIZE 4 * KB
 
 #define NULL (void *)0
 
@@ -29,9 +34,40 @@ create_system_resources(
     uint32_t data_size,
     uint32_t bss_size,
     void *flash_elf_text_section,
-    void *flash_elf_data_section,
+    void *flash_elf_const_section,
     void *flash_elf_bss_section)
 {
+    /* fun note for later: this will point to self, do we need allocated list head any more? */
+    pcb_t *pcb = kalloc(sizeof(pcb_t), NULL);
+    assert(pcb);    /* TODO implement assert that panics if false. */
+
+    /*
+     * This PCB itself is the first thing we've allocated, and becomes the head
+     * of the allocated list.
+     */
+    LL_INSERT(pcb->allocated, pcb - sizeof(heap_region_t)); /* TODO implement LL macros. */
+
+    /*
+     * Make this PCB schedulable.
+     */
+    if (pcb_active == NULL) {
+        pcb_active = pcb;
+    }
+    LL_INSERT_CIRCULAR(pcb_active->sched_next, pcb);    /* TODO implement macro, and should probably insert onto the end of the list..? */
+
+    /*
+     * TODO: Allocate sizeof(data + bss) then copy const into data (or something to that effect, todo figure this out).
+     */
+
+    /*
+     * Initialize special-purpose registers.
+     */
+    pcb->registers.sp = kalloc(STACK_SIZE, pcb);
+    assert(pcb->registers.sp);      /* TODO implement assert that panics if false. */
+    pcb->registers.pc = flash_elf_text_section;
+
+
+
 }
 
 /*
