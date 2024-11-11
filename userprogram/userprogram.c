3e496faf71da35fc1d555db6a732415ee437472c
Alec Bargher <abargher@uchicago.edu>
2024-11-11 15:46:02 -0600

    alec: second user program source in comment

--- userprogram/userprogram.c
+++ userprogram/userprogram.c
@@ -15,14 +15,27 @@ int main() {
   // Main Loop
   while (1) {
     gpio_put(25, 1);  // Set pin 25 to high
-    // printf("LED ON!\n");
-    sleep_ms(delay);  // 0.5s delay
-    // busy_wait_ms(delay);
+    sleep_ms(delay);  // 1.0s delay
 
     gpio_put(25, 0);  // Set pin 25 to low
-    // printf("LED OFF!\n");
-    sleep_ms(delay);  // 0.5s delay
-    // busy_wait_ms(delay);
-    // delay += 100;
+    sleep_ms(delay);  // 1.0s delay
   }
 }
+
+// int main() {
+//   int delay = 1000;
+//   // Initialise I/O
+//   stdio_init_all();
+//   timer_hw->dbgpause = 0;
+
+
+//   // Main Loop
+//   while (1) {
+//     printf("LED ON!\n");
+//     sleep_ms(delay);  // 1.0s delay
+
+//     printf("LED OFF!\n");
+//     sleep_ms(delay);  // 1.0s delay
+//   }
+
+// }
