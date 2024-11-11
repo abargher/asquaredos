6dfdb2830c9fa54e01f79bb719923f73a8de3abc
Alec Bargher <abargher@uchicago.edu>
2024-11-11 15:33:30 -0600

    create userprogram subproject to separate kernel and userprog compilation

---
+++ userprogram/userprogram.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/timer.h"
+#include "hardware/clocks.h"
+#include "hardware/uart.h"
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    // Put your timeout handler code in here
+    return 0;
+}
+
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart1
+#define BAUD_RATE 115200
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+
+
+
+int main()
+{
+    stdio_init_all();
+
+    // Timer example code - This example fires off the callback after 2000ms
+    add_alarm_in_ms(2000, alarm_callback, NULL, false);
+    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+
+    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
+    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
+    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
+
+    // Set up our UART
+    uart_init(UART_ID, BAUD_RATE);
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    
+    // Use some the various UART functions to send out data
+    // In a default system, printf will also output via the default UART
+    
+    // Send out a string, with CR/LF conversions
+    uart_puts(UART_ID, " Hello, UART!\n");
+    
+    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+}
