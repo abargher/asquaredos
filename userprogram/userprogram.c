4b09f20b052ee1926768acf13c5e1b94e0af86ce
Alec Bargher <abargher@uchicago.edu>
2024-11-11 15:39:43 -0600

    alec: userprogram code

--- userprogram/userprogram.c
+++ userprogram/userprogram.c
@@ -1,56 +1,28 @@
 #include <stdio.h>
+
 #include "pico/stdlib.h"
-#include "hardware/timer.h"
-#include "hardware/clocks.h"
-#include "hardware/uart.h"
 
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
-}
+int main() {
+  int delay = 1000;
+  // Initialise I/O
+  stdio_init_all();
+  timer_hw->dbgpause = 0;
 
+  // initialise GPIO (Green LED connected to pin 25)
+  gpio_init(25);
+  gpio_set_dir(25, GPIO_OUT);
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
-#define BAUD_RATE 115200
+  // Main Loop
+  while (1) {
+    gpio_put(25, 1);  // Set pin 25 to high
+    // printf("LED ON!\n");
+    sleep_ms(delay);  // 0.5s delay
+    // busy_wait_ms(delay);
 
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
-
-
-
-int main()
-{
-    stdio_init_all();
-
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
-
-    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
-    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
-
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    gpio_put(25, 0);  // Set pin 25 to low
+    // printf("LED OFF!\n");
+    sleep_ms(delay);  // 0.5s delay
+    // busy_wait_ms(delay);
+    // delay += 100;
+  }
 }
