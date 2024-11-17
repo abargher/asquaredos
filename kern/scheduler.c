76b44d0d8bf4f488a5afd7e7045255ab15717a73
gustrain <gus@waldspurger.com>
2024-11-17 16:12:03 -0600

    prints

--- kern/scheduler.c
+++ kern/scheduler.c
@@ -4,7 +4,12 @@
 
 pcb_t *sched_get_next(void) {
     pcb_t *next_pcb;
+    printf("ready_queue = %p\n", ready_queue);
     DLL_POP(ready_queue, next_pcb, next, prev);
+    printf("ready_queue = %p\n", ready_queue);
     DLL_PUSH(ready_queue, next_pcb, next, prev);
+    printf("ready_queue = %p\n", ready_queue);
+    printf("next_pcb = %p, next_pcb->saved_sp = %p\n", next_pcb, next_pcb->saved_sp);
+
     return next_pcb;
 }
