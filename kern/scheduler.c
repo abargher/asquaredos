f29c681f1b1b93020f0daa8631b73b396547d35a
gustrain <gus@waldspurger.com>
2024-11-17 16:18:36 -0600

    writes

--- kern/scheduler.c
+++ kern/scheduler.c
@@ -1,15 +1,22 @@
 #include "scheduler.h"
 #include "resources.h"
 #include "utils/list.h"
+#include <stdio.h>
 
 pcb_t *sched_get_next(void) {
     pcb_t *next_pcb;
-    printf("ready_queue = %p\n", ready_queue);
+    char buf[128];
+    sprintf(buf, "ready_queue = %p\n", ready_queue);
+    write(stdout, buf);
     DLL_POP(ready_queue, next_pcb, next, prev);
-    printf("ready_queue = %p\n", ready_queue);
+    sprintf(buf, "ready_queue = %p\n", ready_queue);
+    write(stdout, buf);
     DLL_PUSH(ready_queue, next_pcb, next, prev);
-    printf("ready_queue = %p\n", ready_queue);
-    printf("next_pcb = %p, next_pcb->saved_sp = %p\n", next_pcb, next_pcb->saved_sp);
+    sprintf(buf, "ready_queue = %p\n", ready_queue);
+    write(stdout, buf);
+    sprintf(buf, "next_pcb = %p, next_pcb->saved_sp = %p\n", next_pcb, next_pcb->saved_sp);
+    write(stdout, buf);
+    fflush(stdout);
 
     return next_pcb;
 }
